052e9938527ae994b4a1400701a1ee8ff6e768d9
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-08-14 22:52:06 +0530

    Update downloader.sh

--- downloader.sh
+++ downloader.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cloudname=$(cat /cloudvale|sed "s/,/\n/g"|grep value |sed "s/value='//g" |sed "s/')//g")
 threats=$(cat /threats|sed "s/,/\n/g"|grep value |sed "s/value='//g" |sed "s/')//g")
-og="progression.log"
+log="progression.log"
 match="SEED"
 
 aria2c --dir=/home/$cloudname --input-file=/urls.txt --max-concurrent-downloads=1 --connect-timeout=60 --max-connection-per-server=$threats --split=$threats --min-split-size=1M --human-readable=true --download-result=full --file-allocation=none > "$log" 2>&1 &
