0bb850e9f7ecafd0e342541e3ede5b8bf180eaa9
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-08-15 10:58:54 +0530

    Update downloader.sh

--- downloader.sh
+++ downloader.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-cloudname=$(cat /cloudvale|sed "s/,/\n/g"|grep value |sed "s/value='//g" |sed "s/')//g")
-threats=$(cat /threats|sed "s/,/\n/g"|grep value |sed "s/value='//g" |sed "s/')//g")
+cloudname1=$(cat /cloudvale|sed "s/,/\n/g"|grep value |sed "s/value='//g" |sed "s/')//g")
+threats1=$(cat /threats|sed "s/,/\n/g"|grep value |sed "s/value='//g" |sed "s/')//g")
 log="progression.log"
 match="SEED"
 
-aria2c --dir=/home/$cloudname --input-file=/urls.txt --max-concurrent-downloads=1 --connect-timeout=60 --max-connection-per-server=$threats --split=$threats --min-split-size=1M --human-readable=true --download-result=full --file-allocation=none > "$log" 2>&1 &
+aria2c --dir=/home/$cloudname1 --input-file=/urls.txt --max-concurrent-downloads=1 --connect-timeout=60 --max-connection-per-server=$threats1 --split=$threats1 --min-split-size=1M --human-readable=true --download-result=full --file-allocation=none > "$log" 2>&1 &
 pid=$!
 while sleep 5
 do
